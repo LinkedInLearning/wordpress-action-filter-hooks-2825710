299b54d8c6d91bef3387696add6304b3183eb490
Carrie Dils <carriedils@gmail.com>
2020-06-07 12:43:40 -0500

    Create 01_04.php

---
+++ 01_04.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Plugin Name: Hooks Test
+* 
+*/
+
+// Add text to login screen
+function hello_world() {
+	echo "Hello World!";
+}
+// Example of using add_action
+add_action( 'login_header', 'hello_world' );
+
+// Modify URL on login screen
+function change_headerurl( $url ) {
+	$url = "https://carriedils.com";
+	return $url;
+}
+// Example of using add_filter
+add_filter( 'login_headerurl', 'change_headerurl' );
