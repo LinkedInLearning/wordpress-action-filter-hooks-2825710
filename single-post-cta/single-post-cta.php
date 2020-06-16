1ee3c0850ed86e83e539a78027f53e2f5782f672
Carrie Dils <carriedils@gmail.com>
2020-06-16 11:25:50 -0700

    Create single-post-cta.php

---
+++ single-post-cta/single-post-cta.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Plugin Name:       Single Post CTA
+ * Plugin URI:        https://github.com/LinkedInLearning/wordpress-action-filter-hooks-2825710
+ * Description:       Adds sidebar (widget area) to single posts
+ * Version:           0.1
+ * Author:            Carrie Dils
+ * Author URI:        https://carriedils.com
+ * License:           GPL v2+
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       spc
+ */
+
+// If this file is called directly, abort
+if ( !defined( 'ABSPATH' ) ) {
+	die;
+}
