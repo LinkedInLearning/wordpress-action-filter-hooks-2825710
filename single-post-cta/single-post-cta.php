eda96458494af614e8f18fe42dda9afb9e66e52a
Carrie Dils <carriedils@gmail.com>
2020-06-16 11:30:53 -0700

    Update single-post-cta.php

--- single-post-cta/single-post-cta.php
+++ single-post-cta/single-post-cta.php
@@ -29,6 +29,28 @@ function spc_load_stylesheet() {
 // Hook stylesheet
 add_action( 'wp_enqueue_scripts', 'spc_load_stylesheet' );
 
+// Register a custom sidebar
+function spc_register_sidebar() {
+    register_sidebar( array(
+	    'name'          => __( 'Single Post CTA', 'spc' ),
+	    'id'            => 'spc-sidebar',
+	    'description'   => __( 'Displays widget area on single posts', 'spc' ),
+	    'before_widget' => '<div class="widget spc">',
+	    'after_widget'  => '</div>',
+	    'before_title'  => '<h2 class="widgettitle spc-title">',
+	    'after_title'   => '</h2>',
+	) );
+}
+
+// Hook sidebar
+add_action( 'widgets_init', 'spc_register_sidebar' );
+
+
+
+
+
+
+
 
 
 
